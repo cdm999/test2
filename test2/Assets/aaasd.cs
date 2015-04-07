b7077bd7c83c98f5f86caa72c39a55766eb91c7e
jickson98 <jickson98@naver.com>
2015-04-07 16:29:15 +0900

    ㅁㅇㅁㄴㅇㅁㄴㄻ

--- test2/Assets/aaasd.cs
+++ test2/Assets/aaasd.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class aaasd : MonoBehaviour {
 
+	int asa;
+
 	// Use this for initialization
 	void Start () {
 	
+		Debug.Log ("!!!!!");
+
 	}
 	
 	// Update is called once per frame
